0bb6eb0bccc0c829f2342f69f4de2f8145bced2b
hulltn <hulltn@mail.uc.edu>
2020-10-13 05:38:23 +0000

    Added Student File and print method

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Student.print();
+   
+    Student student1 = new Student();
+
+    student1.setName("Arnold Spielberg");
+    student1.setGPA(4.0);
+    student1.setMajor("Electrical Engineering");
+    
+    }
+      
 }
---
+++ Student.java
@@ -0,0 +1,32 @@
+class Student {
+    String name;
+    double gpa;
+    String major;
+
+    String getName() {
+      return name;
+    }
+
+    double getGPA() {
+      return gpa;
+    }
+
+    String getMajor() {
+      return major;
+    }
+
+    void setName(String theName) {
+      name = theName;
+    }
+
+    void setGPA(double theGPA) {
+      gpa = theGPA;
+    }
+
+    void setMajor(String theMajor) {
+      major = theMajor;
+    }
+    public static void print() {
+      System.out.println("The Student's Name is ");
+    }
+  }
